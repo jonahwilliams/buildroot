436de91c5cc4817f115ad471494227c11c5d31ef
Jonah Williams <jonahwilliams@google.com>
2018-06-05 14:12:38 -0700

    Update gn_helpers.py

--- build/gn_helpers.py
+++ build/gn_helpers.py
@@ -10,15 +10,24 @@ class GNException(Exception):
 
 
 def ToGNString(value, allow_dicts = True):
-  """Prints the given value to stdout.
-
+  """Returns a stringified GN equivalent of the Python value.
   allow_dicts indicates if this function will allow converting dictionaries
   to GN scopes. This is only possible at the top level, you can't nest a
   GN scope in a list, so this should be set to False for recursive calls."""
-  if isinstance(value, str):
+  if isinstance(value, basestring):
     if value.find('\n') >= 0:
       raise GNException("Trying to print a string with a newline in it.")
-    return '"' + value.replace('"', '\\"') + '"'
+    return '"' + \
+        value.replace('\\', '\\\\').replace('"', '\\"').replace('$', '\\$') + \
+        '"'
+
+  if isinstance(value, unicode):
+    return ToGNString(value.encode('utf-8'))
+
+  if isinstance(value, bool):
+    if value:
+      return "true"
+    return "false"
 
   if isinstance(value, list):
     return '[ %s ]' % ', '.join(ToGNString(v) for v in value)
@@ -27,8 +36,8 @@ def ToGNString(value, allow_dicts = True):
     if not allow_dicts:
       raise GNException("Attempting to recursively print a dictionary.")
     result = ""
-    for key in value:
-      if not isinstance(key, str):
+    for key in sorted(value):
+      if not isinstance(key, basestring):
         raise GNException("Dictionary key is not a string.")
       result += "%s = %s\n" % (key, ToGNString(value[key], False))
     return result
